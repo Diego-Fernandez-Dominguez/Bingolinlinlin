075657332cf6bb4816aa7def82e3061d7c148e0f
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-03-17 11:13:27 +0100

    Almost

--- src/bingolinlinlin/Card.java
+++ src/bingolinlinlin/Card.java
@@ -1,13 +1,24 @@
 package bingolinlinlin;
 
+import java.util.HashSet;
+import java.util.Random;
+
 public class Card {
+
 	int cardCode = 0;
+
 	int card[][] = new int[3][5];
 
-	public Card(int cardCode) {
+	String name = "Unknown";
+
+	public Card(int cardCode, String name) {
 
 		if (cardCode > 0) {
 			this.cardCode = cardCode;
+		}
+
+		if (name != null && !name.isBlank()) {
+			this.name = name;
 		}
 
 		this.card = generateCard();
@@ -16,9 +27,21 @@ public class Card {
 
 	private int[][] generateCard() {
 
+		Random rand = new Random();
+
+		HashSet<Integer> numbers = new HashSet<Integer>();
+
+		int number;
+		
+		int contI;
+		int contJ;
+
 		int card[][] = new int[3][5];
 
-		return card;
-	}
+		while()
 
+			return card;
+		}
+
+	}
 }
