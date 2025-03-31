986fc27e66b9ca8f5aa2bb5ff2f605b0b4563ab0
am.lozano <am.lozano@106-08.info2>
2025-03-31 11:07:53 +0200

    qwe

--- src/bingolinlinlin/BingoMain.java
+++ src/bingolinlinlin/BingoMain.java
@@ -3,59 +3,91 @@ package bingolinlinlin;
 import java.util.Scanner;
 
 public class BingoMain {
+
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner (System.in);
 
 		int option = -1;
-		
+
 //		System.out.println("Hello world =^b");
-		
+
 		do {
-			
+
 			menu();
-			
+
 			System.out.println("Introduce an option");
 			option = sc.nextInt();
-			
+
 			switch (option) {
-			
-			case 1 -> {
-				
+
+			case 1 -> { // Show card
+
 			}
-			case 2 -> {
-				
+			case 2 -> { // Check card
+
 			}
-			case 3 -> {
-				
+			case 3 -> { // New card
+
 			}
-			case 4 -> {
-				
+			case 4 -> { // Give up
+
 				System.out.println("Leaving game");
-				
+
 			}
 			default -> {
-				
+
 				System.out.println("Wrong option");
-				
+
 			}
 			}
-			
+
 			sc.nextLine();
-			
+
 		} while (option != 4);
-		
+
 		sc.close();
-		
+
 	}
 
 	public static void menu() {
 
-		System.out.println("\u001B[34m" + "1. Show card\r\n"
-				+ "\u001B[33m" + "2. Check card\r\n"
-				+ "\u001B[32m" + "3. New card\r\n"
-				+ "\u001B[31m" + "4. Give up" + "\u001B[0m");
+		System.out.println("\u001B[34m" + "1. Show card\r\n" + "\u001B[33m" + "2. Check card\r\n" + "\u001B[32m"
+				+ "3. New card\r\n" + "\u001B[31m" + "4. Give up" + "\u001B[0m");
 
+	}
+
+	private static String askName() {
+
+		String name;
+		
+		System.out.println("Introduce the user name");
+		name = sc.nextLine();
+		
+		return name;
+	}
+	
+	private static int askCode() {
+		
+		int code;
+		
+		System.out.println("Introduce the code");
+		code = sc.nextInt();
+		
+		sc.nextLine();
+		
+		return code;
+	}
+	
+	public static Card createCard () {
+		
+		Card card = null;
+		
+		String nombre = askName();
+		
+		card = new Card (nombre);
+		
+		return card;
 	}
 
 }
