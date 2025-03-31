312d50c91851796c4a6d18b9ca44f03ae073e217
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-03-31 10:47:23 +0200

    Bingolin updated

--- src/bingolinlinlin/Bingolin.java
+++ src/bingolinlinlin/Bingolin.java
@@ -1,6 +1,7 @@
 package bingolinlinlin;
 
 import java.util.HashSet;
+import java.util.Random;
 
 /**
  * Bingo class.
@@ -10,14 +11,19 @@ public class Bingolin {
 	/**
 	 * Set with different cards from the players.
 	 */
-	private static HashSet<Card> game = new HashSet<>();
+	private HashSet<Card> game = new HashSet<>();
+
+	/**
+	 * Set with different all the numbers have appeared.
+	 */
+	private HashSet<Integer> numbers = new HashSet<>();
 
 	/**
 	 * Function that adds a Card to the set.
 	 * 
 	 * @param card Card that will be added to the set.
 	 */
-	public static void addCard(Card card) {
+	public void addCard(Card card) {
 		game.add(card);
 	}
 
@@ -26,7 +32,7 @@ public class Bingolin {
 	 * 
 	 * @param card Card with a unique id to be deleted from the set.
 	 */
-	public static void deleteCard(Card card) {
+	public void deleteCard(Card card) {
 		game.remove(card);
 	}
 
@@ -35,7 +41,7 @@ public class Bingolin {
 	 * 
 	 * @param id ID from the card to be printed.
 	 */
-	public static void showCard(int id) {
+	public void showCard(int id) {
 
 		Card example = null;
 		for (Card card : game) {
@@ -45,20 +51,47 @@ public class Bingolin {
 			}
 
 		}
-		example.toString();
+		System.out.println(example);
 	}
 
 	/**
 	 * Function that prints all cards.
 	 */
-	public static void showAllCards() {
+	public void showAllCards() {
 
 		for (Card card : game) {
 
-			card.toString();
+			System.out.println(card);
 
 		}
 
+	}
+
+	/**
+	 * Function that checks all cards.
+	 * 
+	 * @param num Num that will be checked.
+	 */
+	public void checkCards(int num) {
+
+		for (Card card : game) {
+			card.checkNumber(num);
+		}
+	}
+
+	/**
+	 * Function that generates a number for the bingo game.
+	 * 
+	 * @return Number generated.
+	 */
+	public int generateNumber() {
+		int number;
+		Random rnd = new Random();
+		do {
+			number = rnd.nextInt(1, 91);
+		} while (numbers.contains(number));
+		numbers.add(number);
+		return number;
 	}
 
 }
